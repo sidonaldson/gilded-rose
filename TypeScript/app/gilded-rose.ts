a52b4cdf557d47a43a0af29a514dc1296b25f47b
Simon Donaldson <simondonaldson@gmail.com>
2020-10-28 19:37:38 +0000

    refactor logic
    Probably going to not use composition in favour of inheritance, it's unlikely the items will diversify a lot.

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -12,51 +12,47 @@ export class Item {
 
 export class NewItem extends Item {
   update(): void {
-    if (
-      this.name != "Aged Brie" &&
-      this.name != "Backstage passes to a TAFKAL80ETC concert"
-    ) {
-      if (this.quality > 0) {
-        if (this.name != "Sulfuras, Hand of Ragnaros") {
-          this.quality = this.quality - 1;
-        }
-      }
-    } else {
-      if (this.quality < 50) {
-        this.quality = this.quality + 1;
-        if (this.name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (this.sellIn < 11) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
-            }
-          }
-          if (this.sellIn < 6) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
-            }
-          }
-        }
-      }
-    }
-    if (this.name != "Sulfuras, Hand of Ragnaros") {
-      this.sellIn = this.sellIn - 1;
-    }
-    if (this.sellIn < 0) {
-      if (this.name != "Aged Brie") {
-        if (this.name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (this.quality > 0) {
-            if (this.name != "Sulfuras, Hand of Ragnaros") {
-              this.quality = this.quality - 1;
-            }
-          }
-        } else {
-          this.quality = this.quality - this.quality;
-        }
-      } else {
+    switch (this.name) {
+      case "Aged Brie":
         if (this.quality < 50) {
           this.quality = this.quality + 1;
         }
-      }
+        this.sellIn = this.sellIn - 1;
+
+        if (this.sellIn < 0 && this.quality < 50) {
+          this.quality = this.quality + 1;
+        }
+        break;
+
+      case "Backstage passes to a TAFKAL80ETC concert":
+        if (this.quality < 50) {
+          this.quality = this.quality + 1;
+          if (this.sellIn < 11 && this.quality < 50) {
+            this.quality = this.quality + 1;
+          }
+          if (this.sellIn < 6 && this.quality < 50) {
+            this.quality = this.quality + 1;
+          }
+        }
+        this.sellIn = this.sellIn - 1;
+        if (this.sellIn < 0) {
+          this.quality = this.quality - this.quality;
+        }
+        break;
+
+      case "Sulfuras, Hand of Ragnaros":
+        //
+        break;
+
+      default:
+        if (this.quality > 0) {
+          this.quality = this.quality - 1;
+        }
+        this.sellIn = this.sellIn - 1;
+        if (this.sellIn < 0 && this.quality > 0) {
+          this.quality = this.quality - 1;
+        }
+        break;
     }
   }
 }
