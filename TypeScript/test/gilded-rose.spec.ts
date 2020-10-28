558d1c86ac9fe3a37ea87f3ba3c8d1247826fd31
Simon Donaldson <simondonaldson@gmail.com>
2020-10-28 16:01:16 +0000

    Test run
    Settled on the TypeScript version
    Fixed unit test
    Breifly looked at updating the deps and trying to run mocka to watch for changes - abandonded in favour of saving time due to nyc/istambul/mocha setup complexities

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -4,7 +4,7 @@ import { Item, GildedRose } from '../app/gilded-rose';
 describe('Gilded Rose', function () {
 
     it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+        const gildedRose = new GildedRose([ new Item('fixme', 0, 0) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].name).to.equal('fixme');
     });
